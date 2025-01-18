ed6713852db7dd590ded9f1ff805da608445659f
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2025-01-19 05:17:47 +0530

    backed delete

--- scraper/urls.py
+++
@@ -1,9 +0,0 @@
-# scraper/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('process_image_url/', views.process_image_url, name='process_image_url'),
-    path('upload_image/', views.upload_image, name='upload_image'),
-]
