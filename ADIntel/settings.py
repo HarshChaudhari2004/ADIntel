fc17d44c842ed3c9583a5277b30a3f8b28ee504a
Harsh Chaudhari <84454436+HarshChaudhari2004@users.noreply.github.com>
2025-01-19 00:39:58 +0530

    django 2nd init

--- ADIntel/settings.py
+++ ADIntel/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scraper',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ADIntel.urls'
@@ -68,6 +72,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ADIntel.wsgi.application'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
